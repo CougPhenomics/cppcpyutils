ba7ecad8943757cd7c304feb783fe612c9cdca7f
Dominik Schneider <dominik.schneider@wsu.edu>
2020-08-06 12:38:17 -0700

    transfer first functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# cppcpyutils" 
+# cppcpyutils
+
+pip install git+https://github.com/CougPhenomics/cppcpyutils.git#egg=cppcpyutils
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='cppcpyutils',
-      version='0.1',
-      description='Python functions for image processing from Compact Plants Phenomics Center',
-      url='http://github.com/CougPhenomics/cppcpyutils',
-      author='Dominik Schneider',
-      author_email='dominik.schneider@wsu.edu',
-      license='MIT',
-      packages=['cppcpyutils'],
-      zip_safe=False)
+setup(
+    name='cppcpyutils',
+    version='0.1',
+    description=
+    'Python functions for image processing from Compact Plants Phenomics Center',
+    url='http://github.com/CougPhenomics/cppcpyutils',
+    author='Dominik Schneider',
+    author_email='dominik.schneider@wsu.edu',
+    license='MIT',
+    packages=['cppcpyutils'],
+    install_requires=["numpy","matplotlib","mpl_toolkits","pandas"],
+    zip_safe=False)
