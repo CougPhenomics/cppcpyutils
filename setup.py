82215cfb9d2fc5b9a3e74cab2d0372c36d2e07df
Dominik Schneider <dominik.schneider@wsu.edu>
2020-08-06 12:22:44 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cppcpyutils',
+      version='0.1',
+      description='Python functions for image processing from Compact Plants Phenomics Center',
+      url='http://github.com/CougPhenomics/cppcpyutils',
+      author='Dominik Schneider',
+      author_email='dominik.schneider@wsu.edu',
+      license='MIT',
+      packages=['cppcpyutils'],
+      zip_safe=False)
