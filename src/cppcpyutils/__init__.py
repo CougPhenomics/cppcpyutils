489128cd9002422e3c57ec7cd9973422d1226da6
Dominik Schneider <dominik.schneider@wsu.edu>
2020-08-27 16:22:28 -0700

    added roi iteration

--- src/cppcpyutils/__init__.py
+++ src/cppcpyutils/__init__.py
@@ -3,3 +3,4 @@ __version__ = '0.1'
 from cppcpyutils import data
 from cppcpyutils import util
 from cppcpyutils import viz
+pixelresolution = None
--- src/cppcpyutils/util/__init__.py
+++ src/cppcpyutils/util/__init__.py
@@ -3,3 +3,5 @@
 from cppcpyutils.util.masked_stats import mean
 from cppcpyutils.util.masked_stats import std
 from cppcpyutils.util.compute import greenness_index
+from cppcpyutils.util.roi import iterate_rois
+from cppcpyutils.util.roi import copy_metadata
