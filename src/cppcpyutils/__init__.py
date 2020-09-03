7f600081b68324fa245e88bf3e010718c1adfe32
Dominik Schneider <dominik.schneider@wsu.edu>
2020-09-03 11:59:39 -0700

    add cli for LT_db_extractor

--- src/cppcpyutils/__init__.py
+++ src/cppcpyutils/__init__.py
@@ -1,5 +1,7 @@
 '''utilities for common tasks in plantcv pipelines'''
-__version__ = '0.2.0'
+__version__ = '0.2.1'
+
+from cppcpyutils import LT_db_extractor
 from cppcpyutils import compute
 from cppcpyutils import io
 from cppcpyutils import roi
