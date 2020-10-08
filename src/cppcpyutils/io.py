9731dc309da436f397e6161d261ca9f6d45300cb
Dominik Schneider <dominik.schneider@wsu.edu>
2020-10-08 12:05:10 -0700

    add regex delimiter and extension kwarg

--- src/cppcpyutils/io.py
+++ src/cppcpyutils/io.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 import pandas as pd
 
 
-def import_snapshots(snapshotdir, camera='psII'):
+def import_snapshots(snapshotdir, camera='psII', ext='png', delimiter='-'):
     """Import snapshots from PSII imaging
 
     Parameter
@@ -15,6 +15,10 @@ def import_snapshots(snapshotdir, camera='psII'):
         path to image directory
     camera : str
         camera label for backwards compatibility (no longer used)
+    ext : str
+        extension of image files
+    delimiter : str
+        either single character delimiter or a regex string
 
     Returns
     -------
@@ -26,17 +30,19 @@ def import_snapshots(snapshotdir, camera='psII'):
 
     """
 
-    fns = find_images(snapshotdir)
-    return get_imagemetadata(fns)
+    fns = find_images(snapshotdir, ext)
+    return get_imagemetadata(fns, delimiter)
 
 
-def find_images(snapshotdir):
+def find_images(snapshotdir, ext):
     """Find png images is specified directory
 
     Parameters
     ----------
     snapshotdir : str
-        directory of .png files
+        directory of image files
+    ext : str
+        extension of image files
 
     Returns
     -------
@@ -54,20 +60,23 @@ def find_images(snapshotdir):
     if not os.path.exists(snapshotdir):
         raise ValueError('the path %s does not exist' % snapshotdir)
 
-    fns = [fn for fn in glob.glob(pathname=os.path.join(snapshotdir, '*.png'))]
+    fns = [fn for fn in glob.glob(pathname=os.path.join(snapshotdir, '*%s' % ext))]
 
     if len(fns) == 0:
-        raise RuntimeError('No files with extension .png were found in the directory specified.')
+        raise RuntimeError('No files with extension %s were found in the directory specified.' % ext)
 
     return(fns)
 
 
-def get_imagemetadata(fns):
+def get_imagemetadata(fns, delimiter):
     """Get image filenames and metadata from filenames
     Parameters
     ----------
         fns : list
             filenames of image files
+        delimiter : str
+            single character or regex to split filename
+
 
     Returns
     -------
@@ -80,11 +89,15 @@ def get_imagemetadata(fns):
         if the filenames do not have 6 pieces delimited by a `-`
 
     """
+    # Compile regex (even if it's only a delimiter character)
+    regex = re.compile(delimiter)
 
     flist = list()
-    for fn in fns:
-        f = re.split('[-]', os.path.splitext(os.path.basename(fn))[0])
-        f.append(fn)
+    for fullfn in fns:
+        fn = os.path.basename(fullfn)
+        fn = os.path.splitext(fn)
+        f = parse_filename(fn[0], delimiter, regex) #if delimiter is a single character it will split filenam with delimiter otherwise uses regex
+        f.append(fullfn)
         flist.append(f)
 
     try:
@@ -94,7 +107,7 @@ def get_imagemetadata(fns):
                             'cameralabel', 'frameid', 'filename'
                         ])
     except ValueError as e:
-        raise ValueError('The filenames did have correctly formated metadata separated by "-".') from e
+        raise ValueError('The filenames did have correctly formated metadata as specified by delimiter argument.') from e
 
     # convert date and time columns to datetime format
     fdf['datetime'] = pd.to_datetime(fdf['timestamp'])
@@ -113,3 +126,32 @@ def get_imagemetadata(fns):
                          'jobdate']).drop(columns=['timestamp'])
 
     return fdf
+
+
+# Filename metadata parser, from plantcv 3.10
+###########################################
+def parse_filename(filename, delimiter, regex):
+    """Parse the input filename and return a list of metadata.
+
+    Args:
+        filename:   Filename to parse metadata from
+        delimiter:  Delimiter character to split the filename on
+        regex:      Compiled regular expression pattern to process file with
+
+    :param filename: str
+    :param delimiter: str
+    :param regex: re.Pattern
+    :return metadata: list
+    """
+
+    # Split the filename on delimiter if it is a single character
+    if len(delimiter) == 1:
+        metadata = filename.split(delimiter)
+    else:
+        metadata = re.search(regex, filename)
+        if metadata is not None:
+            metadata = list(metadata.groups())
+        else:
+            metadata = []
+    return metadata
+###########################################
